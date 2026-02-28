5d069c78505c4a777c16bfb4635e370ea54f05bf
saverio596 <saverio.benedetto96@gmail.com>
2026-02-28 19:49:19 +0100

    Rimossi file ignorati dal repository

--- dist/ProcessoProduzione.d.ts
+++
@@ -1,9 +0,0 @@
-import type { IProdotto, IProcessoProduzione } from './interfaces.js';
-export declare class ProcessoProduzione implements IProcessoProduzione {
-    nomeProcesso: string;
-    descrizione: string;
-    prodottiProduzione: IProdotto[];
-    constructor(nome: string, descrizione: string);
-    aggiungiProdotto(prodotto: IProdotto): void;
-}
-//# sourceMappingURL=ProcessoProduzione.d.ts.map
